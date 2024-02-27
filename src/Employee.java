d6cfce79205d6f669da2c4d8fd1fff6429e64162
ELXKUBANO <sba23110@student.cct.ie>
2024-02-27 17:08:17 +0000

    Email validation Regex
    I have created Regex so that "The value must not be accepted if its length is 3 or less.". If its not valid error message will be printed. Changed this.email = email; to setEmail(email);. This will ensure that any email in the code will have to pass the validation test (Regex)

--- src/Employee.java
+++ src/Employee.java
@@ -1,3 +1,6 @@
+
+import java.util.regex.Pattern;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -26,7 +29,7 @@ public class Employee{
     //Construct with parameters to set custom name
     public Employee(String name, String email){
         this.name = name;
-        this.email = email;
+        setEmail(email);
         this.empNum = nextEmpNum++;
     }
     
@@ -43,5 +46,16 @@ public class Employee{
     //Accessor method for employee unique number
     public int getEmpNum(){
         return empNum;
+    }
+    
+    //Set a new email if it matches valid email pattern as specified in the assigment
+    public void setEmail(String email){
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+    //Check if email matches the pattern
+    if(Pattern.matches(emailRegex, email)){
+        this.email = email;
+    }else{
+        System.out.println("Invalid email format. Hence email not updated.");
+    }
     }
 }
