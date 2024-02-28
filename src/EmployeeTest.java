8095f511ef381a83b3f73741dad1f1aac7df2e59
ELXKUBANO <sba23110@student.cct.ie>
2024-02-28 22:20:16 +0000

    Creation of array list with 3 employee deatils
    I have the three different employees and created array list called projectGroup.

--- src/EmployeeTest.java
+++ src/EmployeeTest.java
@@ -8,5 +8,14 @@
  * @author jakubzaruski
  */
 public class EmployeeTest {
-    
+    public static void main(String[] args){
+        //3 Employee objects from the assigment
+        Employee emp1 = new Employee("Joe", "Bloggs", "jb@gmail.com");
+        Employee emp2 = new Employee("Ann", "Banana", "ab@gmail.com");
+        Employee emp3 = new Employee("Tom", "Thumb", "tt@gmail.com");
+        
+        //An array called projectGroup and stores the 3 employees objects from above
+        Employee[] projectGroup = {emp1, emp2, emp3};
+        
+    }
 }
