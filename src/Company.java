4cb2342dc941ccc1f80e567e9f7d0e42e156faeb
ELXKUBANO <sba23110@student.cct.ie>
2024-02-29 22:47:34 +0000

    Getters and setters for the username/password
    This code enables the creation of manager object that possess all attributes of an employee plus management specific credentials, role access and operations within the system.

--- src/Company.java
+++ src/Company.java
@@ -60,7 +60,26 @@ public class Company {
             this.password = password;
         }
         
-        
+    //Getters and setters for username and password
+    public String getUsername() {
+        return username;
+    }    
+    
+    public void setUsername(String username) {
+        this.username = username;
     }
+    
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    }
+    
+    
+    
             
 }
