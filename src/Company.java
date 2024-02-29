9b7af7aba86208875d2e329339146d8f4129594e
ELXKUBANO <sba23110@student.cct.ie>
2024-02-29 21:51:01 +0000

    Comapny name and employee array list
    Variable string to store company name, array list of employee objects.

---
+++ src/Company.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author jakubzaruski
+ */
+public class Company {
+    //Company name and list of employees
+    private String companyName;
+    private ArrayList<Employee> staff;
+    
+    
+}
